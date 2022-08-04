e8d230fcc27b5dbb3622ab791eef8ed497c0423b
emdy01 <110625900+emdy01@users.noreply.github.com>
2022-08-04 20:55:58 +0200

    Add files via upload

---
+++ Client.py
@@ -0,0 +1,89 @@
+# Modules nécessaires
+import json
+from tkinter import Grid
+from turtle import width
+import guizero as gz
+import getopt
+import sys
+import requests
+
+class GUI():
+    def __init__(self):
+        self.app = gz.App("Application commandes du dossier",height=200)
+        self.title_box = gz.Box(self.app, width="fill", align="top", border=True)
+        gz.Text(self.title_box,text="Panneau de contrôle")
+        self.control_box= gz.Box(self.app, width="fill", align="top", border=True)
+        self.form_box=  gz.Box(self.control_box,layout="grid",width="fill",align="left",border=True)
+        self.boutonUp = gz.PushButton(self.form_box,grid=[0,0],text = "Lever le dossier",width="30",command=self.up)
+        self.boutonDown = gz.PushButton(self.form_box,grid=[0,1],text = "Baisser le dossier",width="30",command=self.down)
+        gz.Text(self.form_box,size=10,text="Pas",grid=[1,0],align="left",width=10)
+        self.pas_ig=self.steps= gz.TextBox(self.form_box,grid=[2,0],text="512",)
+        gz.Text(self.form_box,size=10,text="Position",grid=[1,1],align="left",width=10)
+        self.position_ig=self.position= gz.TextBox(self.form_box,grid=[2,1],text="0")
+        self.status_box = gz.Box(self.app,layout="grid",width="fill",align="bottom",border=True)
+        gz.Text(self.status_box,size=8,text="URL Serveur:",grid=[0,0],align="left")
+        self.url_ig=gz.Text(self.status_box,size=8,text=url,grid=[1,0],align="right")
+        gz.Text(self.status_box,size=8,text="Statut:",grid=[0,1],align="left")
+        self.status_ig=gz.Text(self.status_box,size=8,text="??",grid=[1,1],align="right")
+        
+        urlstatus=url+"/status"
+        response = requests.get(urlstatus)
+        r=json.loads(response.text)
+        self.status_ig.value=r['serveur']
+        self.position_ig.value=r['position']        
+        print()
+        
+
+        
+
+
+ 
+        
+
+
+    def launch(self): # pour le main
+        self.app.display()
+    
+    def up(self):
+        n = self.pas_ig.value
+        #ici controle des valeurs entiers positif
+        urlup=url+"/up/"+str(n)
+        response = requests.get(urlup)
+        r=json.loads(response.text)
+        self.url_ig.value=urlup
+        self.position_ig.value=r['position']
+        self.status_ig.value=r['msg']+" "+ str(response.status_code)
+        
+    def down(self):
+        n = self.pas_ig.value
+        #ici controle des valeurs entiers positif
+        urlup=url+"/down/"+str(n)
+        response = requests.get(urlup)
+        r=json.loads(response.text)
+        self.url_ig.value=urlup
+        self.position_ig.value=r['position']
+        self.status_ig.value=r['msg']+" "+ str(response.status_code)
+    
+   
+    def stopSystem(self):
+        self.app.hide()
+
+def usage():
+    print("--url indique l'url nom de host du serveur  ('http://127.0.0.1:8080' par defaut)")
+    print("-h ou --help imprime ce message")
+
+if __name__ == "__main__":
+    url ="http://127.0.0.1:8080"     #url par default
+    try:                                
+        opts, args = getopt.getopt(sys.argv[1:], "h:p:", ["help","url="])  #le programme supporte 3 paramètres : -h ou --help  -p ou --port et l'url du serveur
+    except getopt.GetoptError:          
+        usage()                         
+        sys.exit(2)                     
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            usage()                     
+            sys.exit()                     
+        elif opt in ("--url"):
+            url = arg               
+    interface = GUI()
+    interface.launch()
---
+++ Serveur.py
@@ -0,0 +1,140 @@
+# Le programme Serveur est le serveur http pilotant le moteur en réponse à des requetes http
+#
+# Principes généraux
+# - initialisation
+#       Lecture des paramètres
+#       initilisation du serveur
+# - nouvelle architecture modulaire
+# - Parsing des requests http
+# - robustesse si parametres ko
+# - logging
+# - echanges en html pas en texte
+# - ordre POST et non pas GET
+# - properties
+# - tests aux limites
+# - sécurité
+# - versionning du code GIT
+
+
+# Modules nécessaires
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import sys
+import json
+import getopt
+
+class MyServer(BaseHTTPRequestHandler):
+
+
+    def _set_headers(self,httpcode):
+        self.send_response(httpcode)                            # 200 pour ok 400 pour erreur 
+        self.send_header('Content-type', 'application/json')    # format du retour en json
+        self.end_headers()
+
+    def usageApi(self):
+        self._set_headers(200)
+        output=json.dumps({ 'usage' : "/status pour statut du serveur, /up/n pour lever dossier, /down/n pour baisser dossier"})
+        self.wfile.write(output.encode())   
+       
+    def do_GET(self):
+        global g_position, MAX_POSITION, MIN_POSITION
+        pathList=self.path.strip('/').split('/') #decoupe le path de l'url dans une liste en supprimant les / des extrémités
+        if len(pathList)==1:
+            if pathList[0]=='':
+                self.usageApi()
+            else:
+                if pathList[0] == "status":
+                    output=json.dumps({'serveur': 'ok', 'position': str(g_position)})    
+                    self._set_headers(200)
+                    self.wfile.write(output.encode())
+                else:
+                    self._set_headers(400)
+                    output=json.dumps({'msg': 'commande invalide', 'position': str(g_position)})
+                    self.wfile.write(output.encode())
+                    self.log_error("commande invalide")
+        else:
+            if len(pathList) != 2 :
+                self._set_headers(400)
+                output=json.dumps({'msg': 'commande invalide 2 arguments attendus', 'order': pathList, 'position': str(g_position)})
+                self.wfile.write(output.encode())
+                self.log_error("commande invalide 2 arguments attendus")
+            else:
+                commande=pathList[0]
+                step=pathList[1]
+                if commande == "up" or  commande == "down" :    
+                    if step.isnumeric():     # si le pas demandé est bien un numerique
+                        n=int(step)
+                        if (n < 1025 and n > 0):
+                            if commande == "up":
+                                if g_position + n > MAX_POSITION :        # si on dépasse le max on va a max
+                                    n = MAX_POSITION-g_position
+                                    g_position = MAX_POSITION
+                                    self.log_message("self.moteur.forward("+str(n)+") MAX_POSITION atteint")  
+                                    msg="leve de "+str(n)+" MAX_POSITION atteint"
+                                else :
+                                    g_position += n
+                                    self.log_message("self.moteur.forward("+str(n)+")")  
+                                    msg="leve de "+str(n)
+                                #self.moteur.forward(n)  
+                            elif  commande == "down":
+                                if g_position - n < MIN_POSITION :        # si on dépasse le min on va a min
+                                    n = g_position - MIN_POSITION
+                                    g_position = MIN_POSITION
+                                    self.log_message("self.moteur.backward("+str(n)+") MIN_POSITION atteint")  
+                                    msg="baisse de "+str(n)+" MIN_POSITION atteint"
+                                else :
+                                    g_position -= n
+                                    self.log_message("self.moteur.backward("+str(n)+")") 
+                                    msg="baisse de "+str(n)
+
+                            self._set_headers(200)
+                            output=json.dumps({'msg': msg,'commande': commande, 'step': step, 'position': str(g_position)})
+                            self.wfile.write(output.encode())
+                        else :
+                            self._set_headers(400)
+                            output=json.dumps({'msg': 'pas du moteur invalide', 'order': commande, 'step': step, 'position': str(g_position)})
+                            self.wfile.write(output.encode())
+                            self.log_error("pas du moteur invalide")
+                    else:
+                        self._set_headers(400)
+                        output=json.dumps({'msg': 'pas du moteur non numerique', 'order': commande, 'step': step, 'position': str(g_position)})
+                        self.wfile.write(output.encode())
+                        self.log_error("pas du moteur non numerique")
+                else:
+                    self._set_headers(400)
+                    output=json.dumps({'msg': 'commande invalide', 'order': commande, 'step': step, 'position': str(g_position)})
+                    self.wfile.write(output.encode())
+                    self.log_error("commande invalide")
+def usage():
+    print("-p ou --port indique le port d'écoute (8080 par defaut)")
+    print("--hostname indique le nom de host d'écoute ('' par defaut)")
+    print("-h ou --help imprime ce message")
+
+
+
+if __name__ == "__main__":
+    serverPort = 8080           #port par defaut
+    hostName =""                #host d'écoute par default
+    g_position = 0                #position du lit, on considère que le lit est à la position 0
+                                #Procédure d'initialisation de la position du lit à 0avec un capteur à implémenter 
+    MAX_POSITION = 1024         #position max du dossier
+    MIN_POSITION = 0            #position min du dossier
+
+    try:                                
+        opts, args = getopt.getopt(sys.argv[1:], "h:p:", ["help","port=","hostname="])  #le programme supporte 3 paramètres : -h ou --help  -p ou --port et hostname
+    except getopt.GetoptError:          
+        usage()                         
+        sys.exit(2)                     
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            usage()                     
+            sys.exit()                     
+        elif opt in ("-p", "--port"):
+            if arg.isnumeric() :
+                serverPort = int(arg)             
+            else :
+                print("port doit etre numerique, valeur par defaut prise")
+        elif opt in ("--hostname"):
+            hostName = arg               
+    print("Server starting http://%s:%s" % (hostName, serverPort))
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    webServer.serve_forever()
