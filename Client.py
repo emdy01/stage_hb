7700efd23bb7faeb6a70a7c3e154b44f9df4400a
emdy01 <110625900+emdy01@users.noreply.github.com>
2022-08-05 12:29:59 +0200

    Update Client.py
    Mise en page et commentaires pour s'y retrouver

--- Client.py
+++ Client.py
@@ -10,22 +10,33 @@ import requests
 class GUI():
     def __init__(self):
         self.app = gz.App("Application commandes du dossier",height=200)
+        
         self.title_box = gz.Box(self.app, width="fill", align="top", border=True)
         gz.Text(self.title_box,text="Panneau de contrôle")
+        
+        #début description des contrôles de la GUI
         self.control_box= gz.Box(self.app, width="fill", align="top", border=True)
         self.form_box=  gz.Box(self.control_box,layout="grid",width="fill",align="left",border=True)
+        
         self.boutonUp = gz.PushButton(self.form_box,grid=[0,0],text = "Lever le dossier",width="30",command=self.up)
         self.boutonDown = gz.PushButton(self.form_box,grid=[0,1],text = "Baisser le dossier",width="30",command=self.down)
-        gz.Text(self.form_box,size=10,text="Pas",grid=[1,0],align="left",width=10)
-        self.pas_ig=self.steps= gz.TextBox(self.form_box,grid=[2,0],text="512",)
-        gz.Text(self.form_box,size=10,text="Position",grid=[1,1],align="left",width=10)
-        self.position_ig=self.position= gz.TextBox(self.form_box,grid=[2,1],text="0")
-        self.status_box = gz.Box(self.app,layout="grid",width="fill",align="bottom",border=True)
-        gz.Text(self.status_box,size=8,text="URL Serveur:",grid=[0,0],align="left")
-        self.url_ig=gz.Text(self.status_box,size=8,text=url,grid=[1,0],align="right")
-        gz.Text(self.status_box,size=8,text="Statut:",grid=[0,1],align="left")
-        self.status_ig=gz.Text(self.status_box,size=8,text="??",grid=[1,1],align="right")
         
+        gz.Text(self.form_box,size=10,text="Pas",grid=[1,0],align="left",width=10)
+        self.pas_ig = self.steps = gz.TextBox(self.form_box,grid=[2,0],text="512",)
+        
+        gz.Text(self.form_box,size=10,text="Position",grid=[1,1],align="left",width=10)
+        self.position_ig = self.position = gz.TextBox(self.form_box,grid=[2,1],text="0")
+        
+        #début de la description des statuts
+        self.status_box = gz.Box(self.app,layout="grid",width="fill",align="bottom",border=True)
+        
+        gz.Text(self.status_box,size=8,text="URL Serveur:",grid=[0,0],align="left")
+        self.url_ig = gz.Text(self.status_box,size=8,text=url,grid=[1,0],align="right")
+        
+        gz.Text(self.status_box,size=8,text="Statut:",grid=[0,1],align="left")
+        self.status_ig = gz.Text(self.status_box,size=8,text="??",grid=[1,1],align="right")
+        
+        # Complétion des statuts avec les infos renvoyées par le serveur
         urlstatus=url+"/status"
         response = requests.get(urlstatus)
         r=json.loads(response.text)
@@ -36,21 +47,26 @@ class GUI():
         self.app.display()
     
     def up(self):
+        #Envoi de la requête
         n = self.pas_ig.value
-        #ici controle des valeurs entiers positif
-        urlup=url+"/up/"+str(n)
-        response = requests.get(urlup)
+        url_up=url+"/up/"+str(n)
+        
+        #Réception de la réponse
+        response = requests.get(url_up)
         r=json.loads(response.text)
-        self.url_ig.value=urlup
+        
+        #Retranscription sur l'interface
+        self.url_ig.value=url_up
         self.position_ig.value=r['position']
         self.status_ig.value=r['msg']+" rc:"+ str(response.status_code)
         
-    def down(self):
+    def down(self): # Même structure que up
         n = self.pas_ig.value
-        #ici controle des valeurs entiers positif
-        urlup=url+"/down/"+str(n)
-        response = requests.get(urlup)
+        url_up=url+"/down/"+str(n)
+        
+        response = requests.get(url_up)
         r=json.loads(response.text)
+        
         self.url_ig.value=urlup
         self.position_ig.value=r['position']
         self.status_ig.value=r['msg']+" rc:"+ str(response.status_code)
